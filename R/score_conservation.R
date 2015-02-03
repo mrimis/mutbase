9ea2e4886744f1967b1abda4d3248c7090f6ca63
Mrinal Mishra <mrimis@utu.fi>
2015-02-03 10:55:23 +0200

    Update score_conservation.R

--- R/score_conservation.R
+++ R/score_conservation.R
@@ -1,14 +1,36 @@
 
-###############################################################################
-# Ambuj Kumar, University of Florida
-# Mrinal Mishra, University of Turku, Finland
-###############################################################################
+################################################################################################################
+#                                                                                                              #
+# Copyright (C) {2014}  {Ambuj Kumar, Kimball-Brain lab group, Biology Department, University of Florida}      #
+#                       {Mrinal Mishra, University of Turku, Finland}                                          #
+#                                                                                                              #
+# This program is free software: you can redistribute it and/or modify                                         #
+# it under the terms of the GNU General Public License as published by                                         #
+# the Free Software Foundation, either version 3 of the License, or                                            #
+# (at your option) any later version.                                                                          #
+#                                                                                                              #
+# This program is distributed in the hope that it will be useful,                                              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                #
+# GNU General Public License for more details.                                                                 #
+#                                                                                                              #
+# This program comes with ABSOLUTELY NO WARRANTY;                                                              #
+# This is free software, and you are welcome to redistribute it                                                #
+# under certain conditions;                                                                                    #
+#                                                                                                              #
+################################################################################################################
+
+
 library(hash)
 PSEUDOCOUNT = .0000001
 
 amino_acids = c('A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', '-')
-iupac_alphabet = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z", "X", "*", "-") 
+iupac_alphabet = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z", "X", "*", "-")
+
 aa_to_index = hash(keys=amino_acids,values=1:length(amino_acids))
+
+
+
 ################################################################################
 # Frequency count
 ################################################################################
@@ -19,7 +41,7 @@ weighted_freq_count_pseudocount=function(col, seq_weights, pc_amount){
   if (length(seq_weights) != length(col)){
     seq_weights = rep(as.double(1.0),length(col))}
 
-  aa_num = 0
+  aa_num = 1
   freq_counts = rep(length(amino_acids),pc_amount) # in order defined by amino_acids
 
   for (aa in amino_acids){
@@ -32,6 +54,8 @@ weighted_freq_count_pseudocount=function(col, seq_weights, pc_amount){
     freq_counts[j] = freq_counts[j] / (sum(seq_weights) + length(amino_acids) * pc_amount)}
   return (freq_counts)
   }
+
+
 ################################################################################
 # Gap Penalty
 ################################################################################
@@ -77,7 +101,7 @@ shannon_entropy <- function(col, sim_matrix, bg_distr, seq_weights, gap_penalty=
     h = 0.
     for (i in 1:length(fc)) {
         if fc[[i]] != 0 {
-            h = h + fc[i] * log(fc[[i]])
+            h = h + fc[[i]] * log(fc[[i]])
         }
     }
     
@@ -115,7 +139,7 @@ property_entropy <- function(col, sim_matrix, bg_distr, seq_weights, gap_penalty
      fc = rep(0, length(property_partition))
      for (p in 1:length(property_partition)) {
          for (aa in property_partition[[p]]) {
-             prop_fc[p] += fc[aa_to_index[aa]]
+             prop_fc[p] = prop_fc[[p]] + fc[[aa_to_index[[aa]]]]
          }
      }
      
@@ -485,24 +509,197 @@ read_scoring_matrix <- function(sm_file) {
     first_line = 1
     row = c()
     list_sm = c()
+    
+    tryCatch( {
+        
+        text <- readLines(sm_file,encoding="UTF-8")
+    
+        for (line in matrix_file) {
+            if (line[1] != '#' and first_line) {
+                first_line = 0
+                if (length(amino_acids) == 0) {
+                    for (c in strsplit(line, " ")) {
+                        aa_to_index[tolower(c)] = aa_index
+                        amino_acids = c(amino_acids, tolower(c))
+                        aa_index = aa_index + 1
+                    }
+                }
+            }
+            elif (line[1] != '#' and first_line == 0) {
+                if (nchar(line) > 1) {
+                    row = strsplit(line, " ")
+                    list_sm = c(list_sm, row)
+                }
+            }
+        }, error = function(err) {
+            return(identity(20))
+        }, finally = {
+            sprintf("Could not load similarity matrix: %s. Using identity matrix...", sm_file)
+        }
+    } )
+    
+    # if matrix is stored in lower tri form, copy to upper
+    if (length(list_sm[[1]]) < 20) {
+        for (i in 1:19) {
+            for (j in i+1:20) {
+                list_sm[i] = c(list_sm[i], list_sm[[j]][i])
+            }
+        }
+    }
+    
+    return(list_sm)
 }
 
 
 
+calculate_sequence_weights <- function(msa) {
+    """ Calculate the sequence weights using the Henikoff '94 method for the given msa. """
+    
+    seq_weights = rep(0, length(msa))
+    
+    for (i in 1:length(msa[[1]])) {
+        freq_counts = rep(0, length(amino_acids))
+        col = c()
+        for (j in 1:length(msa)) {
+            if (msa[[j]][i] != '-') {
+                freq_counts[[aa_to_index[[msa[[j]][i]]]]] = freq_counts[[aa_to_index[[msa[[j]][i]]]]] + 1
+            }
+        }
+        
+        num_observed_types = 0
+        for (j in 1:length(freq_counts)) {
+            if (freq_counts[[j]] > 0) {
+                num_observed_types = num_observed_types + 1
+            }
+        }
+        
+        for (j in 1:length(msa)) {
+            d = freq_counts[[aa_to_index[[msa[[j]][i]]]]] * num_observed_types
+            if d > 0 {
+                seq_weights[j] = seq_weights[[j]] + 1/d
+            }
+        }
+    }
+    
+    for w in range(len(seq_weights)) {
+        seq_weights[w] = seq_weights[[w]] / length(msa[[1]])
+    }
+    
+    return(seq_weights)
+}
 
 
+load_sequence_weights <- function(fname) {
+    seq_weights = c()
+    f = readLines(fname, encoding="UTF-8")
+    for (line in f) {
+        l = strsplit(line, " ")
+        if (line[[1]] != '#' and nchar(l) == 2) {
+            seq_weights = c(seq_weights, l[[2]])
+        }
+    }
+    
+    return(seq_weights)
+}
 
 
+get_column <- (col_num, alignment) {
+    """Return the col_num column of alignment as a list."""
+    col = c()
+    for (seq in alignment) {
+        if (col_num < nchar(seq)) {
+            col = c(col, seq[[col_num]])
+        }
+    }
+    
+    return(col)
+}
 
 
+map_float <- function(vec_Obj) {
+    for (i in 1:length(vec_Obj)) {
+        vec_Obj[i] = as.numeric(vec_Obj[[i]])
+    }
+    
+    return(vec_Obj)
+}
 
 
+get_distribution_from_file <- function(fname) {
+    """ Read an amino acid distribution from a file. The probabilities should be on a single line separated by whitespace in alphabetical order as in amino_acids above. # is the comment character."""
+    
+    distribution = c()
+    f = readLines(fname, encoding="UTF-8")
+    for (line in f) {
+        if (line[[1]] == '#') {
+            line = line[-nchar(line)]
+            distribution = strsplit(line)
+            distribution = map_float(distribution)
+        }
+    }
+    
+    if (.997 > sum(distribution) or sum(distribution) > 1.003) {
+        sprintf("Distribution does not sum to 1. Using default (BLOSUM62) background.")
+        sprintf(sum(distribution))
+        return(c())
+    }
+    
+    return(distribution)
+}
 
 
+replace_element <- function(stringElement, Obj1, Obj2) {
+    new_vec = strsplit(stringElement, "")
+    for (i in 1:length(new_vec[[1]])) {
+        if (new_vec[[1]][[i]] == Obj1) {
+            new_vec[[1]][i] = Obj2
+        }
+    }
+    
+    retString = paste(new_vec[[1]], collapse = "")
+    
+    return(retString)
+}
+read_fasta_alignment=function(filename){
+  """ Read in the alignment stored in the FASTA file, filename. Return two
+    lists: the identifiers and sequences. """
 
+  f = readLines(filename,encoding="UTF-8")
 
+  names = c()
+  alignment = c()
+  cur_seq = ''
 
+  for (line in f){
+    line = line[:-nchar(line)]}
+  if (length(line) == 0){ next }
+  if (line[1] == ';'){next} 
+  if (line[1] == '>'){
+    names.append(replace_element(line[2:].'\r', ''))}
 
+  if (cur_seq != ''){
+    cur_seq = toupper(cur_seq)
+    for (i in 1:length(cur_seq)) {
+      if (cur_seq[i] not in iupac_alphabet){
+        cur_seq = replace_element(cur_seq,cur_seq[i], '-')}}}
+  cur_seq=replace_element(cur_seq,'B','D')
+  cur_seq=replace_element(cur_seq,'Z','Q')
+  cur_seq=replace_element(cur_seq,'X','-')
+  alignment.append(cur_seq)
+  cur_seq = ''
+  else if (line[1] in iupac_alphabet){
+    cur_seq = cur_seq + replace_element(line,'\r', '')}
+
+  # add the last sequence
+  cur_seq = toupper(cur_seq)
+  for (i in 1:length(cur_seq)) {
+    if (cur_seq[i] not in iupac_alphabet){
+      cur_seq = replace_element(cur_seq,cur_seq[i], '-')}}
+  cur_seq=replace_element(cur_seq,'B','D')
+  cur_seq=replace_element(cur_seq,'Z','Q')
+  cur_seq=replace_element(cur_seq,'X','-')
+  alignment.append(cur_seq)
+  return (c(names, alignment))
 
 
 
